5c212742b392f31c8ef0391371f5e339359f301d
KitsuneNoctus <theceintiant@gmail.com>
2024-02-05 16:35:44 -0800

    Project Create

---
+++ CreatingAndCombiningViews/Apple Tutorial/Apple Tutorial/Apple_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_TutorialApp.swift
+//  Apple Tutorial
+//
+//  Created by Henry Calderon on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
