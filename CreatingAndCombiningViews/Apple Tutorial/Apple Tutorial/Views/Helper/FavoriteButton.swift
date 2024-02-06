16f67b6cc95f4e4221da613351bcbeebe4da0aa4
KitsuneNoctus <theceintiant@gmail.com>
2024-02-06 13:32:36 -0800

    Apple tutorial - handling user input

---
+++ CreatingAndCombiningViews/Apple Tutorial/Apple Tutorial/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteButton.swift
+//  Apple Tutorial
+//
+//  Created by Henry Calderon on 2/6/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
