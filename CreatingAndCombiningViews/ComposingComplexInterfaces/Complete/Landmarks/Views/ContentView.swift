9eca5640ca443ad7304bbf1e37369e900d845c21
KitsuneNoctus <theceintiant@gmail.com>
2024-02-07 13:12:31 -0800

    Apple Tutorial - Composing Complex Interfaces

--- CreatingAndCombiningViews/ComposingComplexInterfaces/Complete/Landmarks/Views/ContentView.swift
+++ CreatingAndCombiningViews/ComposingComplexInterfaces/Complete/Landmarks/Views/ContentView.swift
@@ -1,34 +1,34 @@
-//
-//  ContentView.swift
-//  Apple Tutorial
-//
-//  Created by Henry Calderon on 2/5/24.
-//
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view showing featured landmarks above a list of all of the landmarks.
+*/
 
 import SwiftUI
 
 struct ContentView: View {
     @State private var selection: Tab = .featured
-    
+
     enum Tab {
         case featured
         case list
     }
-    
+
     var body: some View {
         TabView(selection: $selection) {
             CategoryHome()
-                .tabItem { 
+                .tabItem {
                     Label("Featured", systemImage: "star")
                 }
                 .tag(Tab.featured)
+
             LandmarkList()
-                .tabItem { 
+                .tabItem {
                     Label("List", systemImage: "list.bullet")
                 }
                 .tag(Tab.list)
         }
-        
     }
 }
 
